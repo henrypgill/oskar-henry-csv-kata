28743b4f327a89386b9ff7eaf5adb7dcf5038d32
oskartyrala <oskartyrala@protonmail.com>
2023-08-17 12:00:08 +0200

    Map txt file into array of arrays

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,28 @@
+import fs from "fs";
+// import csv from "csv";
+// import csv from "fast-csv";
 
+const inputFile = "./src/jokes.txt";
+// const outputFile = './output.csv'
 
+fs.readFile(inputFile, "utf8", (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
 
+    const splitRegex = /\n{2,}/g;
+    const arrOfStrings = data.split(splitRegex);
 
+    const arrOfArrays = arrOfStrings.map((joke) => [joke]);
 
+    console.log(arrOfArrays);
 
+    // const csvStream = csv.format({ headers: true });
+    // csvStream.pipe(process.stdout).on("end", () => process.exit());
+
+    // for (const joke of arrOfStrings) {
+    //     csvStream.write({ header1: joke });
+    // }
+    // csvStream.end();
+});
